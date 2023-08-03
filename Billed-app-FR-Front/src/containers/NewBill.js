4b26f01e7b657d6733a01879a4c02bc4112a1ca8
Alexis Dernoncourt <adernoncourt@insitaction.com>
2023-08-03 20:01:52 +0200

    Fix: [Bug Hunt]-Bills (send file + modal)

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -20,25 +20,33 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const fileType = file.type
+    const acceptedExtensions = ["image/jpeg", "image/jpg", "image/png"]
+    const isAcceptedFile = acceptedExtensions.includes(fileType)
+    
+    if (isAcceptedFile) {
+      const formData = new FormData()
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true,
+          }
+        })
+        .then(({fileUrl, key}) => {
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    } else {
+      e.target.value = ""
+      throw Error('This file is not allowed.')
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
